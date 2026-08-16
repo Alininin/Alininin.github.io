af9c17895851bbd2271061ca4f0731f43b8a70bb
Skixkk <skixkk7@gmail.com>
2026-08-16 18:09:44 +0800

    feat(page): 💜 rewrite index page with purple theme profile content
    - replace default placeholder content with personal profile information
    - retain original badge image links
    - add scoped purple‑theme style
    - no emoji inside page content
    - local image assets reserved for public/images if needed later
    
    No breaking changes, only home page content update.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
+<!--
+ * @Author: Skixkk skixkk7@gmail.com
+ * @Date: 2026-08-16 16:07:54
+ * @LastEditors: Skixkk skixkk7@gmail.com
+ * @LastEditTime: 2026-08-16 18:02:36
+ * @FilePath: \Alininin.github.io\app\app.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<!-- app/app.vue -->
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
